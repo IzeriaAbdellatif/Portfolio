c7db36e65d327416a26eec72938b01dbc6dec4ba
IzeriaAbdellatif <abdellatif.izeria@gmail.com>
2025-12-03 09:34:27 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import elt from "../../Assets/Projects/etl_pipeline.png";
 import rag from "../../Assets/Projects/rag.jpg";
 import telecom from "../../Assets/Projects/telecom_analysis.png";
+import signal from "../../Assets/Projects/signal.png"
 
 function Projects() {
   return (
@@ -37,7 +38,7 @@ Orchestrated the entire ETL process using Apache Airflow for automation and reli
             <ProjectCard
               imgPath={telecom}
               isBlog={false}
-              title="teleco churn prediction"
+              title="machine learning model for telecom churn prediction"
               description="develop a classification model capable of predicting whether a customer is likely to cancel their subscription, based on their past behavior and characteristics."
               ghLink="https://github.com/IzeriaAbdellatif/Telecom-Churn-prediction-using-machine-Learning"
               demoLink="https://drive.google.com/file/d/1qUgWgNV--pCF3iwoNt8Md3ba8wHqktrH/view?usp=sharing"
@@ -50,6 +51,16 @@ Orchestrated the entire ETL process using Apache Airflow for automation and reli
               title="AI Chatbot for Civil Engineering Data Retrieval"
               description="an intelligent chatbot capable of automatically answering questions related to data specific to the field of civil engineering. The system is connected to a database or a set of technical documents (plans, reports, standards, etc.) and allows users (engineers, technicians, students, etc.) to quickly access relevant information via an intuitive conversational interface"
               demoLink="https://drive.google.com/file/d/1-PV4zDaxNQK4ieXdgvrpIZ3eh1ks3y8X/view?usp=sharing"
+              />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={signal}
+              isBlog={false}
+              title="Signal processing model for audio anonymisation"
+              description="build a solution for audio anonymisation and tone of voice masking, utilising advanced signal processing techniques such as time stretching, pitch shifting, noise addition, and frequency filtering. This method aims to preserve audio information while effectively concealing the identity of the audio owner. The efficacy of the proposed solution is evaluated by calculating the EEC and WEC coefficients, providing a quantitative assessment of the quality of the anonymisation. "
+              demoLink="https://drive.google.com/drive/folders/1l9E0HcPcmB7dW5mzP0q13sgWEOy7YcfV?usp=sharing"
+              ghLink="https://github.com/IzeriaAbdellatif/VPC25"
               />
           </Col>
           
